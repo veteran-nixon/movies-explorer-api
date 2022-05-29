0d01e68e17dae77c6ca40c0ff4a7e00d09c455cc
Evgeny Dolganev <dolganev007@yandex.ru>
2022-05-29 17:03:49 +0700

    создать схемы и модели user и movie

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 
+const mongoose = require('mongoose');
+
 const app = express();
 
-const { PORT = 3000 } = process.env;
+const {
+  NODE_ENV,
+  serverdb,
+  PORT = 3000,
+} = process.env;
+
+// адрес базы данных в production-режиме берётся из process.env
+mongoose.connect(NODE_ENV === 'production' ? serverdb : 'mongodb://localhost:27017/bitfilmsdb');
 
 app.listen(PORT, () => {
 // Если всё работает, консоль покажет, какой порт приложение слушает
